46cc7d06b7148fc35d9ecf55c64fc8b8087c53a8
lidrail35 <lidrail35@mail.ru>
2023-04-14 23:34:30 +0300

    feat add methods root, add to BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,17 +6,56 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
+class ListNode {
+  constructor(x) {
+    this.data = x;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.node = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.node;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+add_node(rootNode, addNode) {
+  console.log('222', rootNode.value);
+  console.log('333', addNode);
+  
+  if (rootNode.data > addNode.data) {
+      if (rootNode.left === null) {
+         rootNode.left = addNode;
+      } else {
+        this.add_node(rootNode.left, addNode);
+      }  
+    }  
+    if (rootNode.data < addNode.data) {
+      if (rootNode.right === null) {
+         rootNode.right = addNode;
+      } else {
+        this.add_node(rootNode.right, addNode);
+      }  
+    }            
+}
+
+add(data) {
+  console.log('data', data);
+const temp = new ListNode(data);
+  if (this.node === null) {
+    this.node = temp;
+    return;
+  } else {
+    this.add_node(this.node, temp);
   }
+}
+
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
